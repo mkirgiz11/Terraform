a43938a13fd2ff4d1d1c1c611554b2d873b90d00
mkirgiz11 <mkirgiz11@gmail.com>
2024-02-20 22:07:35 -0500

    session2 additions, providers,versions.etc

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = "ami-0440d3b780d96b29d" #value after argument(ami)
+  instance_type = "t2.micro" #value after argument(instance_type)
 }
 
-# HashiCorp Language
-#Terraform
-# 2 Main Blocks
+### HashiCorp Language
+##Terraform##
+##2 Main Blocks
     # 1.Resource = Block
-    # 2.Data_source
+    # 2.Data_source = Block
 
 #resource = block
     # Resource block is to designed to create and manage resources.
@@ -19,6 +19,17 @@ resource "aws_instance" "web" {
     # Keep in mind it is a resource name. (defined by author)
     # Logical name must be unique
 
-# ami, instance_type = arguments 
+# ami, instance_type = arguments (configurations)
     # (defined by Terraform)
 
+# Argument values are known BEFORE you create the resource(AMI ID etc..)
+# Attribute values are known AFTER you create the resource(Resource ID, ARN etc..)
+
+## Working Directory ##
+# This directory is a place where you declare all the .tf files 
+# and execute your TF commands
+
+#Terraform init
+# Initializes the working directory
+# Terraform Folder is the working directory
+# will read every .tf file in this directory
