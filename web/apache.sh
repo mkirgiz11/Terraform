b36c39f42a4afafad89477945283baa2143aee76
mkirgiz11 <mkirgiz11@gmail.com>
2024-03-05 22:14:51 -0500

    updates

--- web/apache.sh
+++ web/apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo dnf isntall httpd
+sudo dnf install httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "<h1>Mithat's task is done</h1>" >> /var/www/html/index.html
